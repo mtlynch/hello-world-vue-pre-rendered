db4ec76e956c9aef8863f91d759366ab5f77c056
Michael Lynch <git@mtlynch.io>
2019-12-11 09:24:07 -0500

    Adding step 1 version

--- pages/index.vue
+++ pages/index.vue
@@ -2,11 +2,6 @@
   <div>
     <h1>Hello, world!</h1>
     <p>I'm an example of a pre-rendered Vue webpage.</p>
-    <p>
-      To learn more, visit my
-      <a href="https://github.com/mtlynch/hello-world-vue-static">Github repo</a
-      >. or check the <nuxt-link to="/about">about page</nuxt-link>
-    </p>
   </div>
 </template>
 
