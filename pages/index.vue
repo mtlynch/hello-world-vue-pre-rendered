59ddb8b5380e3832ffc0dd3d9e18ae37db915810
Michael Lynch <git@mtlynch.io>
2019-12-11 10:28:35 -0500

    Fixing period placement

--- pages/index.vue
+++ pages/index.vue
@@ -4,8 +4,10 @@
     <p>I'm an example of a pre-rendered Vue webpage.</p>
     <p>
       To learn more, visit my
-      <a href="https://github.com/mtlynch/hello-world-vue-static">Github repo</a
-      >. or check the <nuxt-link to="/about">about page</nuxt-link>
+      <a href="https://github.com/mtlynch/hello-world-vue-static"
+        >Github repo</a
+      >
+      or check the <nuxt-link to="/about">about page</nuxt-link>.
     </p>
   </div>
 </template>
