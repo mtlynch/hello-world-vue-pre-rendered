34ce95f2324a55e71239429ddcf5a734c67fd3ce
Michael Lynch <git@mtlynch.io>
2019-12-11 09:58:43 -0500

    Changing index to include click counter

--- pages/index.vue
+++ pages/index.vue
@@ -2,13 +2,16 @@
   <div>
     <h1>Hello, world!</h1>
     <p>I'm an example of a pre-rendered Vue webpage.</p>
+    <button v-on:click="count++">I have been clicked {{ count }} times</button>
   </div>
 </template>
 
 <script>
 export default {
-  head: {
-    title: "Hello, World!"
+  data: function() {
+    return {
+      count: 0
+    };
   }
 };
 </script>
